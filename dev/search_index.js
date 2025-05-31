6d3710fc1139a6c897a9ca267272173be8b74772
Documenter.jl <documenter@juliadocs.github.io>
2025-05-31 20:28:14 +0000

    build based on 4ad943c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SymbolicCore","page":"Home","title":"SymbolicCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SymbolicCore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
